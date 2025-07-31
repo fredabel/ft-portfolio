5a43263fc25790045133487c62604ed9d920fd32
fredabel <tuazonpred@gmail.com>
2025-07-31 15:31:23 -0400

    Fix hero max width

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,34 +13,24 @@ const Hero: React.FC = () => {
                     className="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75"
                 />
             </div>
-            <div className="mx-auto max-w-3xl pt-32 pb-20 sm:py-40 lg:py-40">
-                <div className="hidden sm:mb-8 sm:flex sm:justify-start">
-                    {/* <img src={'./images/ft_profile.jpeg'} className="w-20 h-20 rounded-full" alt="Frederic Tuazon Profile" /> */}
-                    {/* <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                    Announcing our next round of funding.{' '}
-                    <a href="#" className="font-semibold text-indigo-600">
-                        <span aria-hidden="true" className="absolute inset-0" />
-                        Read more <span aria-hidden="true">&rarr;</span>
-                    </a>
-                    </div> */}
+            <div className="mx-auto max-w-5xl pt-32 pb-20 sm:py-30 lg:py-30">
+                <div className="mt-10">
+                    <h1 className="text-3xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl mb-8">
+                        Frederic Tuazon
+                    </h1>
+                    <h1 className="text-2xl font-semibold tracking-tight text-balance text-gray-800 sm:text-3xl">
+                        Software Engineer | Full Stack Developer
+                    </h1>
+                    <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
+                        I’m a passionate and adaptable software developer with years of experience building web applications. I enjoy designing scalable systems that prioritize user experience and maintainability.
+                    </p>
+                    <div className="mt-10 flex items-center  lg:justify-start sm:justify-center gap-x-6">
+                        <button className="rounded-full cursor-pointer bg-indigo-600 px-5 py-2 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                            Hire Me
+                        </button>
+                    </div>
                 </div>
-            <div className="">
-                <h1 className="text-3xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl mb-8">
-                    Frederic Tuazon
-                </h1>
-                <h1 className="text-2xl font-semibold tracking-tight text-balance text-gray-800 sm:text-3xl">
-                    Software Engineer | Full Stack Developer
-                </h1>
-                <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
-                    I’m a passionate and adaptable software developer with years of experience building web applications. I enjoy designing scalable systems that prioritize user experience and maintainability.
-                </p>
-                <div className="mt-10 flex items-center  lg:justify-start sm:justify-center gap-x-6">
-                    <button className="rounded-full cursor-pointer bg-indigo-600 px-5 py-2 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    >
-                        Hire Me
-                    </button>
-                </div>
-            </div>
             </div>
             <div
             aria-hidden="true"

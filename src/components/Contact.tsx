c1d03d5ac866fd8015635db2bab3a37baa4bacde
fredabel <tuazonpred@gmail.com>
2025-07-31 20:33:39 -0400

    Fix placeholder

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -34,7 +34,7 @@ const Contact: React.FC = () => {
                                 name="first-name"
                                 type="text"
                                 autoComplete="given-name"
-                                placeholder="Type your first name here"
+                                placeholder="John"
                                 className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
                             />
                         </div>
@@ -47,7 +47,7 @@ const Contact: React.FC = () => {
                                 name="last-name"
                                 type="text"
                                 autoComplete="family-name"
-                                placeholder="Type your last name here"
+                                placeholder="Doe"
                                 className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
                             />
                         </div>

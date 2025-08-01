1db4266beae1ba25a6b6f7a40898dbc8db3467d7
fredabel <tuazonpred@gmail.com>
2025-07-31 20:06:28 -0400

    Fix project component layout

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -51,10 +51,10 @@ const Projects : React.FC = () =>{
         <div className="relative isolate border-t border-gray-200 sm:py-20 py-10">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <div className="mx-auto max-w-5xl ">
-                    <p className="mt-2 text-2xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-3xl lg:text-balance">
+                    <p className="mt-2 pl-4 text-2xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-3xl lg:text-balance border-l-5 border-gray-200">
                         My Projects
                     </p>
-                <p className="mt-6 text-lg/8 text-gray-700">
+                <p className="mt-6 text-lg/ text-gray-700 pl-5">
                     I develop modern, scalable web applications using Python (Flask) for backend services and React with Bootstrap for building responsive and user-friendly interfaces. My projects follow REST API standards and are designed with clean, maintainable code using industry best practices, including state management, authentication/authorization, and database integration.
                 </p>
                 </div>
@@ -62,31 +62,25 @@ const Projects : React.FC = () =>{
                     <div className="mx-auto mt-10 grid max-w-xl grid-cols-1 gap-x-8 gap-y-16  lg:mx-0 lg:max-w-none lg:grid-cols-3">
                         {
 							features.map((feature) => (
-                       
 								<div key={feature.name} className="flex max-w-xl flex-col items-start justify-between p-4 rounded shadow-md transition hover:scale-110">
 									<div className="flex items-center gap-x-4 text-xs">
 										<time dateTime="Mar 16, 2025" className="text-gray-500">
 											{feature.date}
 										</time>
-										<a
-										href="#"
-										className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+										<a href="#" className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
 										>
 											Personal
 										</a>
 									</div>
 									<div className="group relative grow mb-2">
 										<h3 className="mt-3 text-lg/6 font-semibold text-gray-900 group-hover:text-indigo-600">
-										<a href={feature.link} target="_blank" >
-											<span className="absolute inset-0" />
-											{feature.name}
-										</a>
+											<a href={feature.link} target="_blank" >
+												<span className="absolute inset-0" />
+												{feature.name}
+											</a>
 										</h3>
 										<p className="mt-2 line-clamp-3 text-sm/6 text-gray-600">{feature.description}</p>
 									</div>
-									{/* <div className="relative mt-8 flex items-center gap-x-4 justify-self-end">
-									
-									</div> */}
 								</div>
                         
                         	))
